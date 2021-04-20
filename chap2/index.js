e0828f16531a3ae3fe9281030336ade2b9e09ec1
healthcarefrontend <himchan9494@gmail.com>
2021-04-20 12:35:29 +0900

    chap2_dom

---
+++ chap2/index.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("title");
+
+// title.innerHTML = "고쳤다";
+
+console.dir(title);
+
+// DOM Document Object Model
+// 자바스크립트는 html에 있는 모든 요소를 가지고 온다
+// 그걸 객체로 바꿔준다
